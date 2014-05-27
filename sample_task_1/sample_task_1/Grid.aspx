b1ee304b9dd26c1fdb644ff59e3b75760fe2ef33
d-michael <rgnkop@gmail.com>
2014-05-27 11:54:35 +0300

    #2 GridView sorting and alt column names

--- sample_task_1/sample_task_1/Grid.aspx
+++ sample_task_1/sample_task_1/Grid.aspx
@@ -9,16 +9,8 @@
 <body>
     <form id="form1" runat="server">    
     <div>
-        <asp:GridView ID="GridView1" runat="server" AllowSorting="True" BackColor="White" BorderColor="#CC9966" BorderStyle="None" BorderWidth="1px" CellPadding="4">
-            <FooterStyle BackColor="#FFFFCC" ForeColor="#330099" />
-            <HeaderStyle BackColor="#990000" Font-Bold="True" ForeColor="#FFFFCC" />
-            <PagerStyle BackColor="#FFFFCC" ForeColor="#330099" HorizontalAlign="Center" />
-            <RowStyle BackColor="White" ForeColor="#330099" />
-            <SelectedRowStyle BackColor="#FFCC66" Font-Bold="True" ForeColor="#663399" />
-            <SortedAscendingCellStyle BackColor="#FEFCEB" />
-            <SortedAscendingHeaderStyle BackColor="#AF0101" />
-            <SortedDescendingCellStyle BackColor="#F6F0C0" />
-            <SortedDescendingHeaderStyle BackColor="#7E0000" />
+        <asp:GridView ID="GridView1" runat="server" AllowSorting="True" allowpaging="true" autogeneratecolumns="true"
+            onsorting="GridView1_Sorting"  OnRowDataBound="GridView1_RowDataBound">
         </asp:GridView>
     </div>
     </form>
--- sample_task_1/sample_task_1/Grid.aspx.cs
+++ sample_task_1/sample_task_1/Grid.aspx.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+//temp
+using System.Data;
+
 using DAL.DataSet1TableAdapters;
 using BAL;
 
@@ -14,9 +17,60 @@ namespace sample_task_1
         protected void Page_Load(object sender, EventArgs e)
         {
             BooksBAL booksLogic = new BooksBAL();
-            //booksTableAdapter booksAdapter = new booksTableAdapter();
             GridView1.DataSource = booksLogic.GetData();
             GridView1.DataBind();
         }
+
+        public SortDirection GridViewSortDirection
+        {
+            get
+            {
+                if (ViewState["sortDirection"] == null)
+                    ViewState["sortDirection"] = SortDirection.Ascending;
+
+                return (SortDirection)ViewState["sortDirection"];
+            }
+            set { ViewState["sortDirection"] = value; }
+        }
+
+        protected void GridView1_Sorting(object sender, GridViewSortEventArgs e)
+        {
+            String sortExpression = e.SortExpression;
+
+            if (GridViewSortDirection == SortDirection.Ascending)
+            {
+                GridViewSortDirection = SortDirection.Descending;
+                SortGridView(sortExpression, " DESC");
+            }
+            else
+            {
+                GridViewSortDirection = SortDirection.Ascending;
+                SortGridView(sortExpression, " ASC");
+            }
+
+        }
+
+        private void SortGridView(string sortExpression, string direction)
+        {
+            BooksBAL booksLogic = new BooksBAL();
+            DataView myDataView = new DataView(booksLogic.GetData());
+            myDataView.Sort = sortExpression + direction;
+            GridView1.DataSource = myDataView;
+            GridView1.DataBind();
+        }
+
+        protected void GridView1_RowDataBound(object sender, GridViewRowEventArgs e)
+        {
+            if (e.Row.RowType == DataControlRowType.Header)
+            {
+                LinkButton LnkHeaderText = e.Row.Cells[0].Controls[0] as LinkButton;
+                LnkHeaderText.Text = "Book";
+                LinkButton LnkHeaderText2 = e.Row.Cells[1].Controls[0] as LinkButton;
+                LnkHeaderText2.Text = "Author";
+                LinkButton LnkHeaderText3 = e.Row.Cells[2].Controls[0] as LinkButton;
+                LnkHeaderText3.Text = "Temp_name";
+            }
+        }
     }
+
 }
